7cf3febe86498e6686337347807206941dabeaf5
ふぁ <yuki@yuki0311.com>
2023-05-29 02:33:23 +0900

    update docs
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  build: {
+    outDir: "docs",
+  },
   plugins: [react(), tsconfigPaths()],
 });
